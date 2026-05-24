5e55519fbd9578584622dad5ea55042cfd69c872
Mausimux <56847436+Mausimux@users.noreply.github.com>
2026-05-24 06:47:18 +0200

    Neovim config now supports emmylua_ls too

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,7 +2,29 @@ vim.pack.add {
 	Gh('neovim/nvim-lspconfig'),
 }
 
--- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/lua_ls.lua
+-- Load Neovim's Lua API for both most popular Lua LSPs
+vim.lsp.config('emmylua_ls', {
+	on_init = function(client)
+		if client.workspace_folders then
+			local path = client.workspace_folders[1].name
+			if path ~= vim.fn.stdpath('config') and (vim.uv.fs_stat(path .. '/.emmyrc.json') or vim.uv.fs_stat(path .. '/.luarc.json'))
+				then return end
+			end
+
+		client.config.settings.emmylua = vim.tbl_deep_extend('force', client.config.settings.emmylua or {}, {
+			runtime = {
+				version = 'LuaJIT',
+				path = {
+					'lua/?.lua',
+					'lua/?/init.lua'
+				},
+			},
+			workspace = { library = vim.api.nvim_get_runtime_file('', true) }
+		})
+	end,
+})
+
+
 vim.lsp.config('lua_ls', {
 	on_init = function(client)
 
