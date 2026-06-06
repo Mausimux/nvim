f64b6031a90b8f40496f793410b31555ea522cf5
Mausimux <56847436+Mausimux@users.noreply.github.com>
2026-06-06 03:14:04 +0200

    refactor

--- init.lua
+++ init.lua
@@ -2,14 +2,15 @@ vim.g.mapleader = ' '
 
 Gh = function(repo) return 'https://github.com/' .. repo end
 
+vim.opt.clipboard = "unnamedplus"
+vim.opt.confirm = true
+vim.opt.cursorline = true
+vim.keymap.set("n", "<Esc>", '<cmd>nohlsearch<CR>')
+
 require('indent')
 require('line_numbers')
 require('whitespace')
-require('clipboard')
-require('cursorline')
-require('confirm')
 require('arrow_keys')
-require('clear_search')
 require('highlight_yank')
 require('lsp')
 require('finder')
